cdd80ceb3fa7c59ddea69192a0d2fd5c12110d8d
Prashuk Ajmera <prashuk.ajmera@gmail.com>
2021-01-02 10:47:40 -0600

    WebView Programming

--- WebView/WebViewProg/WebViewProg/ViewController.swift
+++ WebView/WebViewProg/WebViewProg/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+                
+        let myWebView = WKWebView(frame: CGRect(
+                                        x: 0,
+                                        y: -50,
+                                        width: UIScreen.main.bounds.width,
+                                        height: UIScreen.main.bounds.height + 50
+                                    ))
+        myWebView.scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        self.view.addSubview(myWebView)
+                
+        let url = URL(string: "https://prashuk.co")
+        let urlRequest = URLRequest(url: url!)
+        myWebView.load(urlRequest)
     }
-
-
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
 
